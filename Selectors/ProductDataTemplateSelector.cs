33672bf0911ecafc946e9423da63edb4eeebb118
gdemartino93 <g.demartino9393@gmail.com>
2025-03-23 21:59:13 +0100

    defining layout based on item property

---
+++ Selectors/ProductDataTemplateSelector.cs
@@ -0,0 +1,18 @@
+﻿using MAUI_CollectionView.MVVM.Models;
+
+namespace MAUI_CollectionView.Selectors
+{
+    public class ProductDataTemplateSelector : DataTemplateSelector
+    {
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            Product product = item as Product;
+            if (!product.HasOffer)
+            {
+                Application.Current.Resources.TryGetValue("ProductStyle", out var productStyle);
+                return productStyle as DataTemplate;
+            }
+            return new DataTemplate();
+        }
+    }
+}
