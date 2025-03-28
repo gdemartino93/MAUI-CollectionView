6b2624456b5be2e3cbebe7905af03ed589974d57
gdemartino93 <g.demartino9393@gmail.com>
2025-03-28 21:23:13 +0100

    detecting value scroll

--- MVVM/Views/ProductsView.xaml.cs
+++ MVVM/Views/ProductsView.xaml.cs
@@ -1,4 +1,5 @@
 using MAUI_CollectionView.MVVM.ViewModels;
+using System.Diagnostics;
 
 namespace MAUI_CollectionView.MVVM.Views;
 
@@ -9,4 +10,15 @@ public partial class ProductsView : ContentPage
 		InitializeComponent();
 		BindingContext = new ProductsViewModel();
 	}
+
+    private void CollectionView_Scrolled(object sender, ItemsViewScrolledEventArgs e)
+    {
+		Debug.WriteLine($"primo elemento: {e.FirstVisibleItemIndex}");
+        Debug.WriteLine($"secondo elemento: {e.CenterItemIndex}");
+        Debug.WriteLine($"terzo elemento: {e.LastVisibleItemIndex}");
+        Debug.WriteLine($"delta orzzontale: {e.HorizontalDelta}");
+        Debug.WriteLine($"delta verticale: {e.VerticalDelta}");
+        Debug.WriteLine($"offset vertical: {e.VerticalOffset}");
+        Debug.WriteLine($"offset orizzontale: {e.VerticalOffset}");
+    }
 }
